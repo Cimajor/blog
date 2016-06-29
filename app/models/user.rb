f08238996bc84551fae92782a6cb8f6a3a3d2fc2
Cimajor <milevskiy@gera-it.com>
2016-06-29 21:38:59 +0300

    “DZ”

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
 	has_many :posts
+	validates :first_name, :password, presence: true
+	validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
